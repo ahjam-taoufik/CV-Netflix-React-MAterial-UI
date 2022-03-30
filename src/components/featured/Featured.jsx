73549075b7c9a7d8ef6cb7a8ad5c0353c2ba0646
taoufik ahjam <taoufikahjam@gmail.com>
2022-03-30 12:00:01 +0200

    add featured component

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,58 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import "./featured.scss";
+
+export default function Featured({ type }) {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img
+           
+            src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+            alt=""
+          />
+      <div className="info">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/4221/4221419.png"
+          alt=""
+        />
+       
+        <span className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
+          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
+          temporibus eum quisquam.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
@@ -6,11 +7,9 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-           width="100%"
-            src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            alt=""
-          />
+      <Featured />
+      {/* <Featured type="movie"/> */}
+      {/* <Featured type="TVseries"/> */}
      
     </div>
   );
